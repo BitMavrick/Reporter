47de1b1b1f8609ab126fd2fd6f1a276b368c3452
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-02 13:38:57 +0600

    Templating done

---
+++ resources/views/user/404.blade.php
@@ -0,0 +1,27 @@
+<x-user.master>
+
+    <x-slot name="title">
+        {{ $title ?? 'Blog | Reporter'}}
+    </x-slot>
+
+    <x-user.partials.navbar />
+
+    <main>
+        <section class="py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10 mx-auto text-center">
+                        <img loading="lazy" decoding="async" src="/user/images/404.png" alt="404" class="img-fluid mb-4"
+                            width="500" height="350">
+                        <h1 class="mb-4">Page Not Found!</h1>
+                        <a href="index.html" class="btn btn-outline-primary">Back To Home</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <x-user.partials.footer />
+
+
+</x-user.master>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('user.index');
 });
+
+Route::get('article/', function () {
+    return view('user.article');
+});
+
+Route::get('404/', function () {
+    return view('user.404');
+});
+
+Route::get('category/', function () {
+    return view('user.category');
+});
+
+Route::get('about/', function () {
+    return view('user.about');
+});
+
+Route::get('privacy/', function () {
+    return view('user.privacy-policy');
+});
+
+Route::get('contact/', function () {
+    return view('user.contact');
+});
