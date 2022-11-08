9bd09aaa2cf98c541fae25571cd85c4a2e13ea43
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-08 13:08:36 +0600

    Make another partials for ajax

---
+++ resources/views/components/user/partials/ajax.blade.php
@@ -0,0 +1,18 @@
+<!-- Jquery -->
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"
+    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+<!-- Ajax Script-->
+<script>
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+</script>
+
+<script>
+$(document).ready(function() {
+    alert('Mehedi Hasan');
+});
+</script>
