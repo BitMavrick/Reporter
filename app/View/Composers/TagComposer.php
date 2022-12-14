7420cc0c2c0d31841e3a5d9091a5cf75cf2004f6
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-12-15 00:50:11 +0600

    Done making hot topics

--- app/View/Composers/TagComposer.php
+++ app/View/Composers/TagComposer.php
@@ -3,15 +3,29 @@
 namespace App\View\Composers;
 
 use Illuminate\View\View;
-use App\Models\User_blog;
+use App\Models\Blog_tag;
 
 class TagComposer
 {
 
     public function compose(View $view)
     {
-        //dd('Hello This is a test 2');
-        echo "Hello This is a test 2";
-        $view->with('the_value', "mehedi hasan");
+        $all_tags = Blog_tag::all();
+
+        $the_tags = [];
+        $counter = 1;
+        foreach ($all_tags as $tag) {
+            $tag_counter = Blog_tag::where('tag_name', $tag->tag_name)->count();
+            $the_tags[$tag->tag_name] = $tag_counter;
+
+            if ($counter == 12) {
+                break;
+            }
+            $counter++;
+        }
+
+        arsort($the_tags);
+
+        $view->with('top_tags', $the_tags);
     }
 }
--- resources/views/components/user/partials/sidebar.blade.php
+++ resources/views/components/user/partials/sidebar.blade.php
@@ -140,37 +140,21 @@
                     </div>
                 </div>
             </div>
+            @if(isset($top_tags))
             <div class="col-lg-12 col-md-6">
                 <div class="widget">
-                    <h2 class="section-title mb-3">Categories</h2>
+                    <h2 class="section-title mb-3">Hot Topics</h2>
                     <div class="widget-body">
                         <ul class="widget-list">
-                            <li><a href="#!">computer<span class="ml-auto">(3)</span></a>
+                            @foreach($top_tags as $name => $value)
+                            <li><a href="{{ route('tag.filter', $name) }}">{{ $name }}</a>
                             </li>
-                            <li><a href="#!">cruises<span class="ml-auto">(2)</span></a>
-                            </li>
-                            <li><a href="#!">destination<span class="ml-auto">(1)</span></a>
-                            </li>
-                            <li><a href="#!">internet<span class="ml-auto">(4)</span></a>
-                            </li>
-                            <li><a href="#!">lifestyle<span class="ml-auto">(2)</span></a>
-                            </li>
-                            <li><a href="#!">news<span class="ml-auto">(5)</span></a>
-                            </li>
-                            <li><a href="#!">telephone<span class="ml-auto">(1)</span></a>
-                            </li>
-                            <li><a href="#!">tips<span class="ml-auto">(1)</span></a>
-                            </li>
-                            <li><a href="#!">travel<span class="ml-auto">(3)</span></a>
-                            </li>
-                            <li><a href="#!">website<span class="ml-auto">(4)</span></a>
-                            </li>
-                            <li><a href="#!">hugo<span class="ml-auto">(2)</span></a>
-                            </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
             </div>
+            @endif
         </div>
     </div>
 </div>
