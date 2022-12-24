b557ad08c6817c4037388cc0bb5f1203e2d06093
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-12-24 15:00:08 +0600

    Done article section in admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -62,7 +62,6 @@ class AdminController extends Controller
 
     public function delete_blog(Request $request)
     {
-        dd($request->all());
         $blog = Blog::where('id', $request->blog_id)->first();
         $blog->delete();
         return redirect()->route('super.articles');
