e47156ff43484cbce65cb9c2d11afb71e60c0283
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-12-24 15:16:45 +0600

    Done making admin panel

--- resources/views/admin/articles.blade.php
+++ resources/views/admin/articles.blade.php
@@ -26,7 +26,7 @@
                             <div class="page-title">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                                    <li class="breadcrumb-item active">Users</li>
+                                    <li class="breadcrumb-item active">Articles</li>
                                 </ol>
                             </div>
                         </div>
