c70623f8e4625f1bbbe60172d3bf765d9032147d
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-14 20:03:42 +0600

    Clean Up

--- resources/views/components/admin/partials/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="col-lg-12">
-        <div class="footer">
-            <p>2023 © Admin Panel. - <a href="https://github.com/TheAlter71">Mehedi Hasan</a></p>
-        </div>
-    </div>
-</div>
